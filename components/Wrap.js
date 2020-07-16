2b002cc540f2d2c16c5bba9827d6f6f708651d38
Sjoerd Boerrigter <sjoerd@trendwerk.nl>
2020-07-16 15:59:32 +0200

    :construction: Wrap component

---
+++ components/Wrap.js
@@ -0,0 +1,9 @@
+export default function ({ children, className }) {
+  return (
+    <section className={`px-4 md:px-8 w-full ${className}`}>
+      <div className="w-full max-w-screen-xl mx-auto">
+        { children }
+      </div>
+    </section>
+  )
+}
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import Layout from '../components/Layout'
+import Wrap from '../components/Wrap'
 import { getPosts } from '../lib/posts'
 import { getPages } from '../lib/pages'
 
@@ -11,33 +12,35 @@ export default function ({ pages, posts }) {
           <meta type="description" value="Page description" key="description" />
         </Head>
 
-        <h1>
-          Hello there 🙂
-        </h1>
+        <Wrap>
+          <h1>
+            Hello there 🙂
+          </h1>
 
-        <h2>
-          Here are some pages
-        </h2>
+          <h2>
+            Here are some pages
+          </h2>
 
-        <ul>
-          {pages.edges.map(({ node }) => (
-            <li key={node.id}>
-              {node.title}
-            </li>
-          ))}
-        </ul>
+          <ul>
+            {pages.edges.map(({ node }) => (
+              <li key={node.id}>
+                {node.title}
+              </li>
+            ))}
+          </ul>
 
-        <h2>
-          And here are some posts
-        </h2>
+          <h2>
+            And here are some posts
+          </h2>
 
-        <ul>
-          {posts.edges.map(({ node }) => (
-            <li key={node.id}>
-              {node.title}
-            </li>
-          ))}
-        </ul>
+          <ul>
+            {posts.edges.map(({ node }) => (
+              <li key={node.id}>
+                {node.title}
+              </li>
+            ))}
+          </ul>
+        </Wrap>
     </Layout>
   )
 }
