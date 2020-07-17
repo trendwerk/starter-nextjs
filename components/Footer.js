fb56803fc68c332c1eb03027a7e0824055a1e948
Sjoerd Boerrigter <sjoerd@trendwerk.nl>
2020-07-17 10:17:22 +0200

    :construction: Footer minor thing

--- components/Footer.js
+++ components/Footer.js
@@ -4,7 +4,7 @@ export default function ({ site }) {
   return (
     <Wrap className="bg-gray-800 mt-auto py-6 text-gray-400">
       <div className="text-center w-full">
-        &copy; copyright {new Date().getFullYear()} - {site.title}
+        &copy; copyright {new Date().getFullYear()} &middot; {site.title}
       </div>
     </Wrap>
   )
