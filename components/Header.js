a092cb1d51b08c9cd0f5f3fd0c724e3b4d6c9e1d
Sjoerd Boerrigter <sjoerd@trendwerk.nl>
2020-07-17 15:46:39 +0200

    :construction: Logo

--- components/Header.js
+++ components/Header.js
@@ -1,11 +1,14 @@
-import Wrap from 'components/Wrap'
 import Link from 'next/link'
+import Logo from 'components/Logo'
+import Wrap from 'components/Wrap'
 
 export default function ({ site }) {
   return (
-    <Wrap className="bg-gray-800 mb-8 py-6 text-white">
+    <Wrap className="mb-8 py-4 border-b border-gray-200">
       <Link href="/">
-        <a className="font-serif font-bold text-xl">
+        <a className="font-serif font-bold text-xl flex items-center">
+          <Logo className="text-brand-600 h-10 w-10 mr-4 flex-none" />
+
           {site.title}
         </a>
       </Link>
---
+++ components/Logo.js
@@ -0,0 +1,5 @@
+export default function ({ className }) {
+  return (
+    <svg className={className} viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg"><path d="M50 5C14 5 5 14 5 50s9 45 45 45 45-9 45-45S86 5 50 5z" fill="currentColor"/></svg>
+  )
+}
