e9895206d014442f6f3ac1d058259c58a39d8eca
Sjoerd Boerrigter <sjoerd@trendwerk.nl>
2020-07-17 23:16:15 +0200

    :construction: Footer nomargin prop

--- components/Footer.js
+++ components/Footer.js
@@ -2,7 +2,7 @@ import Wrap from 'components/Wrap'
 
 export default function({ site }) {
   return (
-    <Wrap className="bg-gray-800 mt-auto py-6 text-gray-400">
+    <Wrap noMargin className="bg-gray-800 mt-auto py-6 text-gray-400">
       <div className="text-center w-full">
         &copy; copyright {new Date().getFullYear()} - {site.title}
       </div>
