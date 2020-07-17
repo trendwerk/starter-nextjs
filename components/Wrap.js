2c41453902e3166a6a71b54ee03bf8d8fd843bf7
Sjoerd Boerrigter <sjoerd@trendwerk.nl>
2020-07-17 23:16:01 +0200

    :construction: Wrap margin

--- components/Wrap.js
+++ components/Wrap.js
@@ -1,8 +1,9 @@
-export default function({ children, className, width=false }) {
+export default function({ children, className, width=false, noMargin=false }) {
   return (
     <section className={`px-5 w-full ${className}`}>
       <div
         className={`
+          ${noMargin ? 'mb-8 sm:mb-12' : ''}
           ${width ? '' : 'max-w-screen-xl'}
           last:mb-0
           mx-auto
