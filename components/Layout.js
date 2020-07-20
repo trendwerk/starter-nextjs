c8d3b976a5a665366bd010d4007763916252bfde
Sjoerd Boerrigter <sjoerd@trendwerk.nl>
2020-07-20 13:46:14 +0200

    :truck: Menu > Navbar

--- components/Layout.js
+++ components/Layout.js
@@ -1,13 +1,13 @@
 import Context from 'components/Context'
 import Footer from 'components/Footer'
 import Head from 'components/Head'
-import Menu from 'components/Menu'
+import Navbar from 'components/Navbar'
 
 export default function({ app, children }) {
   return (
     <Context.Provider value={{ app: app }}>
       <Head />
-      <Menu />
+      <Navbar />
       {children}
       <Footer />
     </Context.Provider>
