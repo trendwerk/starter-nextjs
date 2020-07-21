cce8787ffc8aecbc477a33831d689f25b9babf32
Sjoerd Boerrigter <sjoerd@trendwerk.nl>
2020-07-21 21:49:53 +0200

    :truck: Navbar > Navigation

--- components/Layout.js
+++ components/Layout.js
@@ -1,14 +1,14 @@
 import Context from 'lib/Context'
 import Footer from 'components/Footer'
 import Head from 'components/Head'
-import Navbar from 'components/Navbar'
+import Navigation from 'components/Navigaiton'
 
 export default function ({ context, children }) {
   return (
     <Context.Provider value={context}>
       <Head />
       <div className="flex flex-col min-h-screen">
-        <Navbar />
+        <Navigation />
         {children}
         <Footer />
       </div>
