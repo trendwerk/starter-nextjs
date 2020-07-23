9963f2f433392ae296568c0029e5144dd14f6dc0
Luuk de Vlieger <info@luuk.site>
2020-07-23 10:39:25 +0200

    :fire: Remove unused import

--- components/Menu.js
+++ components/Menu.js
@@ -5,7 +5,6 @@ import Hamburger from 'hamburger-react'
 import Link from 'components/Link'
 import Logo from 'components/Logo'
 import { getMenu } from 'lib/menu'
-import tailwind from 'tailwind.config'
 
 export default () => {
   const [isOpen, setOpen] = useState(false)
