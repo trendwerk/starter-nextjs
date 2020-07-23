85df9489f14f96d4fb44fa9ca44368a31dc101da
Sjoerd Boerrigter <sjoerd@trendwerk.nl>
2020-07-23 14:12:35 +0200

    :lipstick: Break words in content and title

--- components/Content.js
+++ components/Content.js
@@ -1,7 +1,7 @@
 export default function ({ content }) {
   return (
     <article
-      className="prose max-w-none mb-8 last:mb-0 overflow-hidden"
+      className="prose max-w-none mb-8 last:mb-0 break-words"
       dangerouslySetInnerHTML={{ __html: content }}
     />
   )
