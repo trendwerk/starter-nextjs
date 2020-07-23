df8e9600f7aebd91a3cfc30533fc6f9de3b2e924
Luuk de Vlieger <info@luuk.site>
2020-07-23 15:03:43 +0200

    :recycle: Layout

--- components/Layout.js
+++ components/Layout.js
@@ -2,14 +2,12 @@ import Data from 'components/Data'
 import Footer from 'components/Footer'
 import Menu from 'components/Menu'
 
-export default function ({ data, children }) {
-  return (
+export default ({ data, children }) => (
+  <div className="flex flex-col min-h-screen">
     <Data.Provider value={{ app: data.app }}>
-      <div className="flex flex-col min-h-screen">
-        <Menu items={data.menuItems} />
-        {children}
-        <Footer />
-      </div>
+      <Menu items={data.menuItems} />
+      {children}
+      <Footer />
     </Data.Provider>
-  )
-}
+  </div>
+)
