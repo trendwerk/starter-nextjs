d409953b33c96de117d5f5db958dfb4370b8e3e6
Luuk de Vlieger <info@luuk.site>
2020-07-24 12:27:32 +0200

    :construction: Search input

--- components/Search.js
+++ components/Search.js
@@ -1,1 +1,8 @@
-export default () => <div className="hidden lg:flex items-center">[search]</div>
+export default () => (
+  <div className="hidden lg:flex items-center">
+    <input
+      type="search"
+      className="w-40 border px-4 py-2 ml-4 rounded-full shadow-inner"
+    />
+  </div>
+)
