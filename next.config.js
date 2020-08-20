812df8209099a6263840f08479015cfdac403091
Luuk de Vlieger <info@luuk.site>
2020-08-20 10:11:07 +0200

    :sparkles: Admin redirect
    Closes #39

--- next.config.js
+++ next.config.js
@@ -1,5 +1,14 @@
 const transpiled = require('next-transpile-modules')(['lodash-es'])
 
+const redirects = async () => [
+  {
+    source: '/admin',
+    destination: `${process.env.WP_URL}/wp/wp-admin/`,
+    permanent: false,
+  },
+]
+
 module.exports = transpiled({
   env: { URL: process.env.URL, WP_URL: process.env.WP_URL },
+  redirects,
 })
