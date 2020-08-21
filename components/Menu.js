4df5fd69af5fa70fe2be95cf427cf362bf9534b3
Luuk de Vlieger <info@luuk.site>
2020-08-21 14:17:50 +0200

    :sparkles: Highlight current menu item + parent
    Closes #59

--- components/Menu.js
+++ components/Menu.js
@@ -4,10 +4,11 @@ import Link from 'components/Link'
 import Search from 'components/Search'
 import Logo from 'components/Logo'
 import getMenu from 'utils/getMenu'
+import { useRouter } from 'next/router'
+import getLink from 'utils/getLink'
 
 const Menu = ({ items }) => {
   const [isOpen, setOpen] = useState(false)
-  const menu = getMenu('MAIN', items)
 
   return (
     <div
@@ -58,94 +59,7 @@ const Menu = ({ items }) => {
           lg:w-auto
         `}
       >
-        {menu.map((item) => {
-          const children =
-            item.childItems.nodes.length > 0 && item.childItems.nodes
-
-          return (
-            <div
-              className="
-              flex
-              flex-col
-              group
-              justify-center
-              relative
-              text-gray-300
-              lg:h-full
-              lg:text-gray-800
-            "
-              key={item.id}
-            >
-              <Link
-                href={item.href}
-                onClick={() => setOpen(false)}
-                className="
-                  flex
-                  font-semibold
-                  hover:text-white
-                  items-center
-                  px-5
-                  py-4
-                  lg:h-full
-                  lg:hover:text-brand-600
-                  lg:px-4
-                  lg:py-0
-                "
-              >
-                {item.label}
-              </Link>
-
-              {children && (
-                <div
-                  className="
-                  -mt-2
-                  flex
-                  flex-col
-                  mb-3
-                  lg:-translate-x-1/2
-                  lg:absolute
-                  lg:bg-gray-800
-                  lg:divide-gray-600
-                  lg:divide-y
-                  lg:group-hover:flex
-                  lg:hidden
-                  lg:left-1/2
-                  lg:mb-0
-                  lg:mt-2
-                  lg:px-5
-                  lg:pt-3
-                  lg:pb-4
-                  lg:rounded
-                  lg:top-16
-                  lg:transform
-                  lg:w-64
-                  lg:z-10
-                "
-                >
-                  {children.map((item) => (
-                    <Link
-                      href={item.href}
-                      onClick={() => setOpen(false)}
-                      className="
-                        hover:text-white
-                        px-5
-                        py-2
-                        text-sm
-                        lg:px-0
-                        lg:py-3
-                        lg:text-gray-300
-                      "
-                      key={item.id}
-                    >
-                      {item.label}
-                    </Link>
-                  ))}
-                </div>
-              )}
-            </div>
-          )
-        })}
-
+        <Items items={items} setOpen={setOpen} />
         <Search />
       </div>
     </div>
@@ -153,3 +67,108 @@ const Menu = ({ items }) => {
 }
 
 export default Menu
+
+const Items = ({ items, setOpen }) => {
+  const menu = getMenu('MAIN', items)
+  const { asPath } = useRouter()
+
+  const getChildren = (item) => item.childItems.nodes.length ? item.childItems.nodes : false
+
+  const isCurrent = (item) => {
+    const equal = (item) => asPath === getLink(item.href).as
+
+    if (equal(item)) {
+      return true
+    }
+
+    return getChildren(item) ? getChildren(item).some(equal) : false
+  }
+
+  return menu.map((item) => (
+      <div
+        className={`
+      flex
+      flex-col
+      group
+      justify-center
+      relative
+      lg:h-full
+    `}
+        key={item.id}
+      >
+        <Link
+          href={item.href}
+          onClick={() => setOpen(false)}
+          className={`
+          flex
+          font-semibold
+          hover:text-white
+          items-center
+          px-5
+          py-4
+          lg:h-full
+          lg:hover:text-brand-600
+          lg:px-4
+          lg:py-0
+          ${
+            isCurrent(item)
+              ? 'text-blue-300 lg:text-blue-800'
+              : 'text-gray-300 lg:text-gray-800'
+          }
+        `}
+        >
+          {item.label}
+        </Link>
+
+        {getChildren(item) && (
+          <div
+            className="
+          -mt-2
+          flex
+          flex-col
+          mb-3
+          lg:-translate-x-1/2
+          lg:absolute
+          lg:bg-gray-800
+          lg:divide-gray-600
+          lg:divide-y
+          lg:group-hover:flex
+          lg:hidden
+          lg:left-1/2
+          lg:mb-0
+          lg:mt-2
+          lg:px-5
+          lg:pt-3
+          lg:pb-4
+          lg:rounded
+          lg:top-16
+          lg:transform
+          lg:w-64
+          lg:z-10
+        "
+          >
+            {getChildren(item).map((item) => (
+              <Link
+                href={item.href}
+                onClick={() => setOpen(false)}
+                className={`
+                ${true ? 'ef' : 'ef'}
+                hover:text-white
+                px-5
+                py-2
+                text-sm
+                lg:px-0
+                lg:py-3
+                ${isCurrent(item) ? 'text-blue-300' : 'text-gray-300'}
+              `}
+                key={item.id}
+              >
+                {item.label}
+              </Link>
+            ))}
+          </div>
+        )}
+      </div>
+    )
+  )
+}
