f7c44e2411de5f4126dbfd00e76a7b02bac8bcc4
Harold Angenent <harold@trendwerk.nl>
2020-08-24 11:42:20 +0200

    ✨ Load more blogposts
    Fixes #7

--- pages/blog.js
+++ pages/blog.js
@@ -1,3 +1,4 @@
+import { useState, useEffect } from 'react'
 import { fetchData, mainQuery } from 'utils/api'
 import Head from 'components/Head'
 import Layout from 'components/Layout'
@@ -5,8 +6,58 @@ import Post from 'components/Post'
 import Title from 'components/Title'
 import Wrap from 'components/Wrap'
 
+const buildPostsQuery = (cursor = '') => `
+  posts(first: 2, after: "${cursor}") {
+    pageInfo {
+      endCursor
+      hasNextPage
+    }
+    edges {
+      node {
+        id
+        title
+        uri
+        dateFormatted
+        summary
+        fields {
+          summaryTitle
+          summary
+          summaryImage {
+            url:sourceUrl
+          }
+          headerImage {
+            url:sourceUrl
+          }
+        }
+      }
+    }
+  }
+`
+
 const Blog = (data) => {
-  const posts = data.posts.edges
+  const [posts, setPosts] = useState(data.posts.edges)
+  const [pageInfo, setPageInfo] = useState(data.posts.pageInfo)
+  const [loading, setLoading] = useState(false)
+  
+  useEffect(() => {
+    if (loading) {
+      const getMore = async () => {
+        const result = await fetchData(`
+          query MorePosts {
+            ${buildPostsQuery(pageInfo.endCursor)}
+          }
+        `).catch(() => setLoading(false))
+
+        if (result) {
+          setLoading(false)
+          setPosts(posts.concat(result.posts.edges))
+          setPageInfo(result.posts.pageInfo)
+        }
+      }
+
+      getMore()
+    }
+  }, [loading])
 
   return (
     <Layout data={data}>
@@ -22,6 +73,10 @@ const Blog = (data) => {
             <p>There are no blog posts yet.</p>
           </div>
         )}
+
+        {pageInfo.hasNextPage && (
+          <a className={`${loading ? 'bg-gray-500 text-gray-300' : 'bg-gray-800 hover:bg-gray-700 text-white'} px-3 py-3 font-bold flex justify-center cursor-pointer`} onClick={() => setLoading(true)}>Load more posts</a>
+        )}
       </Wrap>
     </Layout>
   )
@@ -32,27 +87,7 @@ export default Blog
 export async function getStaticProps() {
   const data = await fetchData(`
     query Blog {
-      posts(first: 10) {
-        edges {
-          node {
-            id
-            title
-            uri
-            dateFormatted
-            summary
-            fields {
-              summaryTitle
-              summary
-              summaryImage {
-                url:sourceUrl
-              }
-              headerImage {
-                url:sourceUrl
-              }
-            }
-          }
-        }
-      }
+      ${buildPostsQuery()}
       ${mainQuery}
     }
   `)
