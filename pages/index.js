eea23220fdb8b1853d09b1828d21b8d3a4b25de3
Sjoerd Boerrigter <sjoerd@trendwerk.nl>
2020-08-25 19:40:35 +0200

    :bug: homepage query

--- pages/index.js
+++ pages/index.js
@@ -34,6 +34,7 @@ export async function getStaticProps() {
       pages: pageByTemplate(where: { template: "page_on_front" }) {
         nodes {
           ${pageQuery}
+        }
       }
       ${mainQuery}
     }
