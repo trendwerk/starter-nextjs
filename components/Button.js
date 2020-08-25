40e235c7e6d200dc8b8ad862067f065bfed16804
Luuk de Vlieger <info@luuk.site>
2020-08-25 13:03:24 +0200

    :sparkles: Submit option for buttons

--- components/Button.js
+++ components/Button.js
@@ -1,35 +1,45 @@
 import Link from 'components/Link'
 
-const Button = function ({ children, className = '', href, large = false }) {
-  return (
-    <Link
-      className={`
-          bg-brand-600
-          duration-200
-          font-semibold
-          hover:bg-brand-700
-          inline-block
-          rounded-lg
-          shadow-md
-          text-center
-          text-white
-          transition-bg
-          ${
-            large
-              ? `
-            px-8
-            py-4
-          `
-              : `
-            px-6
-            py-3
-            text-sm
-          `
-          }
-          ${className}
-      `}
-      href={href}
-    >
+const Button = function ({
+  children,
+  className = '',
+  href,
+  submit,
+  onClick,
+  large = false,
+}) {
+  const classes = `
+    bg-brand-600
+    duration-200
+    font-semibold
+    hover:bg-brand-700
+    inline-block
+    rounded-lg
+    shadow-md
+    text-center
+    text-white
+    transition-bg
+    ${
+      large
+        ? `
+      px-8
+      py-4
+    `
+        : `
+      px-6
+      py-3
+      text-sm
+    `
+    }
+    ${className}
+  `
+
+  return submit ? (
+    <button className={classes} onClick={onClick}>
+      {children}
+    </button>
+  ) : (
+    <Link className={classes} href={href} onClick={onClick}>
       {children}
     </Link>
   )
