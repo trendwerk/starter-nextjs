875ce9401c2832ecf1f5cf7861fe89c480e20e11
Luuk de Vlieger <info@luuk.site>
2020-08-27 16:13:29 +0200

    :lipstick: Submenu

--- components/Submenu.js
+++ components/Submenu.js
@@ -3,11 +3,11 @@ import Link from './Link'
 export default function Submenu({ items, title }) {
   return (
     <>
-      <h2>{title}</h2>
-      <ul>
+      <h3 className="mb-6">{title}</h3>
+      <ul className="list-none border-b">
         {items.map((item) => (
-          <li key={item.id}>
-            <Link href={item.href}>{item.label}</Link>
+          <li key={item.id} className="my-0">
+            <Link className="py-3 px-3 border-t hover:bg-gray-100 flex" href={item.href}>{item.label}</Link>
           </li>
         ))}
       </ul>
