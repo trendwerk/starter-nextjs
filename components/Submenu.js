d662db3ba393c5882613819031131386928dc2f0
Luuk de Vlieger <info@luuk.site>
2020-08-27 16:06:23 +0200

    :construction: Links in submenu
    #58

--- components/Submenu.js
+++ components/Submenu.js
@@ -1,3 +1,16 @@
+import Link from './Link'
+
 export default function Submenu({ items, title }) {
-  return <h2>{title}</h2>
+  return (
+    <>
+      <h2>{title}</h2>
+      <ul>
+        {items.map((item) => (
+          <li key={item.id}>
+            <Link href={item.href}>{item.label}</Link>
+          </li>
+        ))}
+      </ul>
+    </>
+  )
 }
