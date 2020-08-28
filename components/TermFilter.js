60e5f2ec220411646e378f24c4e17dcce1f10403
Harold Angenent <harold@trendwerk.nl>
2020-08-28 12:21:48 +0200

    💄 Style filter

--- components/TermFilter.js
+++ components/TermFilter.js
@@ -14,12 +14,13 @@ export default function TermFilter({ terms, title, onChange }) {
 
   return (
     <div>
-      <h3>{title}</h3>
-      <ul className="list-none">
+      <h3 className="mb-6">{title}</h3>
+      <ul className="list-none border-b">
         {terms.map(({ node }) => (
-          <li key={node.id}>
-            <label className="cursor-pointer">
+          <li key={node.id} className="m-0">
+            <label className="cursor-pointer flex items-center py-3 px-3 border-t hover:bg-gray-100">
               <input
+                className="mr-2"
                 type="checkbox"
                 checked={active.indexOf(node.slug) !== -1}
                 onChange={event => event.target.checked ? setActive([...active, node.slug]) : setActive(active.filter(item => item !== node.slug))}
