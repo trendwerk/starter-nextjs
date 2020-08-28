e72ed11cfb6a559b2990dc37b4be67d64cd38c20
Luuk de Vlieger <info@luuk.site>
2020-08-28 04:31:08 +0200

    :art: Format

--- components/Submenu.js
+++ components/Submenu.js
@@ -7,7 +7,12 @@ export default function Submenu({ data }) {
       <ul className="list-none border-b">
         {data.childItems.nodes.map((item) => (
           <li key={item.id} className="my-0">
-            <Link className="py-3 px-3 border-t hover:bg-gray-100 flex" href={item.href}>{item.label}</Link>
+            <Link
+              className="py-3 px-3 border-t hover:bg-gray-100 flex"
+              href={item.href}
+            >
+              {item.label}
+            </Link>
           </li>
         ))}
       </ul>
