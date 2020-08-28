7d1073a21c92d772a397d6c00dcc2207b807bbab
Harold Angenent <harold@trendwerk.nl>
2020-08-28 10:51:39 +0200

    🚧 Add checkboxes and active state

--- components/TermFilter.js
+++ components/TermFilter.js
@@ -1,10 +1,23 @@
+import { useState } from 'react'
+
 export default function TermFilter({ terms, title }) {
+  const [active, setActive] = useState([])
+
   return (
     <div>
       <h3>{title}</h3>
-      <ul>
+      <ul className="list-none">
         {terms.map(({ node }) => (
-          <li key={node.id}>{node.name}</li>
+          <li key={node.id}>
+            <label className="cursor-pointer">
+              <input
+                type="checkbox"
+                checked={active.indexOf(node.slug) !== -1}
+                onChange={event => event.target.checked ? setActive([...active, node.slug]) : setActive(active.filter(item => item !== node.slug))}
+              />
+              {node.name}
+            </label>
+          </li>
         ))}
       </ul>
     </div>
