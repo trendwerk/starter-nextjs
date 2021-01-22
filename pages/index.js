2d589f4750306d23ff946ee8c039cc4de26f57b4
Sjoerd Boerrigter <sjoerd@trendwerk.nl>
2021-01-22 11:46:35 +0100

    :art: Simplify index.js

--- pages/index.js
+++ pages/index.js
@@ -1,31 +1,14 @@
 import generalQuery from 'queries/generalQuery'
 import getFromApi from 'functions/getFromApi'
-import Layout from 'components/Layout'
 import Page from 'pages/[...page]'
 import pageQuery from 'queries/pageQuery'
-import Wrap from 'components/Wrap'
 
 export default function Home(data) {
-  if (data.pages.nodes.length) {
-    return <Page data={{ ...data, post: data.pages.nodes[0] }} />
+  if (!data.pages.nodes.length) {
+    return <p>Please select a homepage in WordPress.</p>
   }
 
-  return (
-    <Layout data={data}>
-      <Wrap className="text-center">
-        <p>
-          Please select a static homepage in your{' '}
-          <a
-            className="link"
-            href={`${process.env.CMS_URL}/wp/wp-admin/options-reading.php`}
-          >
-            reading settings
-          </a>{' '}
-          to show the homepage.
-        </p>
-      </Wrap>
-    </Layout>
-  )
+  return <Page data={{ ...data, post: data.pages.nodes[0] }} />
 }
 
 export async function getStaticProps() {
