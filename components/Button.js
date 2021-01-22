6331d9ae6eb754755d10b10e4b7da769d6dfcb49
Sjoerd Boerrigter <sjoerd@trendwerk.nl>
2021-01-22 11:46:02 +0100

    :art: Button

--- components/Button.js
+++ components/Button.js
@@ -10,19 +10,21 @@ export default function Button({
   large = false,
 }) {
   const classNames = classnames(
-    disabled
-      ? ['bg-brand-400', 'text-brand-200']
-      : ['bg-brand-600', 'hover:bg-brand-700', 'text-white'],
-    'duration-200',
-    'font-semibold',
-    'inline-block',
-    'rounded',
-    'shadow-md',
-    'text-center',
-    'transition-colors',
-    large ? 'px-8 py-4' : 'px-6 py-3 text-sm',
+    `
+      duration-200
+      font-semibold
+      inline-block
+      rounded
+      shadow-md
+      text-center
+      transition-colors
+    `
     !href && (disabled ? 'cursor-not-allowed' : 'cursor-pointer'),
     className
+    disabled
+      ? ['bg-brand-400 text-brand-200']
+      : ['bg-brand-600 hover:bg-brand-700 text-white'],
+    large ? 'px-8 py-4' : 'px-6 py-3 text-sm',
   )
 
   return href ? (
