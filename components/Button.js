fbdab2716116aec393ebe818f4d2af8c9517e5ff
Sjoerd Boerrigter <sjoerd@trendwerk.nl>
2021-03-01 20:05:14 +0100

    :bug: Button

--- components/Button.js
+++ components/Button.js
@@ -18,13 +18,13 @@ export default function Button({
       shadow-md
       text-center
       transition-colors
-    `
+    `,
     !href && (disabled ? 'cursor-not-allowed' : 'cursor-pointer'),
-    className
+    className,
     disabled
       ? ['bg-brand-400 text-brand-200']
       : ['bg-brand-600 hover:bg-brand-700 text-white'],
-    large ? 'px-8 py-4' : 'px-6 py-3 text-sm',
+    large ? 'px-8 py-4' : 'px-6 py-3 text-sm'
   )
 
   return href ? (
